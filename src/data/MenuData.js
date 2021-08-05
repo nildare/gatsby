d62e099acfbd8b16c72ebffd2770e7cf1e5e554d
nildare <52755147+nildare@users.noreply.github.com>
2021-08-05 12:39:18 +0530

    Add files via upload

---
+++ src/data/MenuData.js
@@ -0,0 +1,6 @@
+export const menuData = [
+    { title: "Trips", Link: "#trips" },
+    { title: "From Us", Link: "#about" },
+    { title: "Offers", Link: "#offers" },
+    { title: "Contact Us", Link: "#contact" },
+]
